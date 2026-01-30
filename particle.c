d3e41a79cb220e0cb43169002c0cae589a2d6ce7
Thiago <thiacefilho@gmail.com>
2026-01-30 12:03:11 -0300

    multiplas particulas, colisão com parede

---
+++ particle.c
@@ -0,0 +1,108 @@
+#include "raylib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+// gcc -Wall -Wextra -g3 -std=c2x -lm particle.c -o output/particle_sim -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 && ./output/particle_sim
+
+#define WIDTH 900
+#define HEIGHT 600
+#define FPS 100
+
+// definições da simulação
+#define PARTICLE_NUM 15
+#define INIT_RADIUS 50
+#define INIT_VELOCITY 5
+
+typedef struct
+{
+   float x, y, rad;
+   float velocity_x, velocity_y;
+} Particle;
+
+void initialize_particles(Particle *particles)
+{
+    int range = (INIT_VELOCITY - (-INIT_VELOCITY) + 1);
+    /* inicia a particula com posições aleatórias e com velocidade aleatória 
+        entre (-velocidade inicial) e velocidade inicial */
+    for(int i=0; i<PARTICLE_NUM; i++)
+    {
+        particles[i] = (Particle){(rand()%WIDTH), (rand()%HEIGHT), 
+            INIT_RADIUS, 
+            (rand()%range + (-INIT_VELOCITY)), 
+            (rand()%range + (-INIT_VELOCITY))};
+    }
+}
+
+void update_particle(Particle *particle)
+{
+    particle->x += particle->velocity_x;
+    particle->y += particle->velocity_y;
+
+    float x = particle->x;
+    float y = particle->y;
+    float rad = particle->rad;
+
+    // colisão com as paredes
+    if((x-rad) < 0)
+    {
+        particle->x = rad;
+        particle->velocity_x = -particle->velocity_x;
+    }
+    if((x+rad) > WIDTH)
+    {
+        particle->x = WIDTH - rad;
+        particle->velocity_x = -particle->velocity_x;
+    }
+    if((y-rad) < 0)
+    {
+        particle->y = rad;
+        particle->velocity_y = -particle->velocity_y;
+    }
+    if((y+rad) > HEIGHT)
+    {
+        particle->y = HEIGHT - rad;
+        particle->velocity_y = -particle->velocity_y;
+    }
+}
+
+void draw_particles(Particle *particles)
+{
+    for(int i=0; i<PARTICLE_NUM; i++)
+    {
+        DrawCircle(particles[i].x, particles[i].y, particles[i].rad, WHITE);
+    }
+}
+
+void update_environment(Particle *particles)
+{
+    for(int i=0; i<PARTICLE_NUM; i++)
+    {
+        update_particle(&particles[i]);
+    }
+}
+
+int main()
+{
+    InitWindow(WIDTH, HEIGHT, "Simulação de particulas");
+    SetTargetFPS(FPS);
+    srand(time(NULL));
+
+    Particle *particles = (Particle*)malloc(PARTICLE_NUM * sizeof(Particle));
+    initialize_particles(particles);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawFPS(10,10);
+
+        update_environment(particles);
+
+        draw_particles(particles);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
